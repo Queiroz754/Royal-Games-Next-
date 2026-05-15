99fa70e80e3b4afdd01c5dd2b3b6de153429f6cd
Queiroz754 <allan754@outlook.com>
2026-05-15 11:00:23 -0300

    :construction: Começando CSS da pagina login

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,21 +1,25 @@
+import style from "./login.module.css"
+
 const Login = () => {
     return (
         <>
-            <main>
-                <img src="../imgs/mulher login.svg" alt="" />
-                <div>
-                    <img src="../imgs/Logo Header.svg" alt="" />
-                    <form action="">
-                        <div>
-                            <label htmlFor="email">E-mail</label>
-                            <input type="text" name="email"/>
-                        </div>
-                        <div>
-                            <label htmlFor="senha">Senha</label>
-                            <input type="password" name="senha" />
-                            <button>Entrar</button>
-                        </div>
-                    </form>
+            <main id={style.main}>
+                <div id={style.container_login}>
+                    <img src="../imgs/mulher login.svg" alt="" />
+                    <div>
+                        <img src="../imgs/Logo Header.svg" alt="" />
+                        <form action="">
+                            <div>
+                                <label htmlFor="email">E-mail</label>
+                                <input type="text" name="email" />
+                            </div>
+                            <div>
+                                <label htmlFor="senha">Senha</label>
+                                <input type="password" name="senha" />
+                                <button>Entrar</button>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </main>
         </>
